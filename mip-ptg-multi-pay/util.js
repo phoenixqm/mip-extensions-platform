4ca99314d7aa777995d04d0b901c21a406ae470c
Qi Ming <phoenixqm@gmail.com>
2018-06-16 17:29:14 +0800

    add store in multi pay

--- mip-ptg-multi-pay/util.js
+++ mip-ptg-multi-pay/util.js
@@ -46,6 +46,88 @@ define(function (require) {
         }
     };
 
+
+
+    /**
+     * 小小的封装下 ls
+     *
+     * @type {Object}
+     */
+    util.store = {
+
+        /**
+         * 存储 key 前缀
+         *
+         * @type {string}
+         */
+        prefix: 'mip-login-xzh:sessionId:',
+
+        /**
+         * 获取 key
+         *
+         * @param  {string} key 键值
+         *
+         * @return {string}
+         */
+        getKey: function (key) {
+            return util.store.prefix + key;
+        },
+
+        /**
+         * 检查是否支持 ls
+         *
+         * @type {boolean}
+         */
+        support: function () {
+            var support = true;
+            try {
+                window.localStorage.setItem('lsExisted', '1');
+                window.localStorage.removeItem('lsExisted');
+            } catch (e) {
+                support = false;
+            }
+            return support;
+        }(),
+
+        /**
+         * 获取缓存数据
+         *
+         * @param  {string} key 数据名称
+         * @return {string}
+         */
+        get: function (key) {
+            if (util.store.support) {
+                return localStorage.getItem(util.store.getKey(key));
+            }
+        },
+
+        /**
+         * 设置缓存数据
+         *
+         * @param {string} key   数据名称
+         * @param {string} value 数据值
+         * @param {UTC} expires 过期时间
+         * @return {string}
+         */
+        set: function (key, value, expires) {
+            if (util.store.support) {
+                localStorage.setItem(util.store.getKey(key), value);
+            }
+        },
+
+        /**
+         * 删除缓存数据
+         *
+         * @param  {string} key 数据名称
+         * @return {string}
+         */
+        remove: function (key) {
+            if (util.store.support) {
+                return localStorage.removeItem(util.store.getKey(key));
+            }
+        }
+    };
+
     /**
      * 发送 POST 请求
      *
